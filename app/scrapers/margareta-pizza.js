39f6d987c021fd32431151178ff862a33549b878
janos.mayer <mjanos10@gmail.com>
2018-07-27 14:42:07 +0200

    Update getting URLs for margareta-pizza

--- app/scrapers/margareta-pizza.js
+++ app/scrapers/margareta-pizza.js
@@ -6,19 +6,23 @@ const axios = require('axios');
 const config = require('config');
 const pizzaPlaceConfig = config.util.toObject(config.get('pizzaPlaces.margaretaPizza'));
 
+const hasPizzaWordInUrl = str => str.split('.hu/')[1].includes('pizza');
+
+const getListOfPizzaUrls = xmlAsArray => {
+	return xmlAsArray.urlset.url.reduce((allUrls, url) => {
+		if (url.priority._text === '0.6' && hasPizzaWordInUrl(url.loc._text)) {
+			allUrls.push(url.loc._text);
+		}
+		return allUrls;
+	}, []);
+};
+
 const scrape = async () => {
 	try {
 		
 		const { data } = await axios.get(pizzaPlaceConfig.sitemapUrl);
 		const result = xmlToJs.xml2js(data, { compact: true, ignoreAttributes: true, ignoreDeclaration: true });
-		const pizzaUrls = result.urlset.url.map(url => {
-			return {
-				loc: url.loc._text,
-				priority: url.priority._text,
-			};
-		}).filter(url => {
-			return url.priority === '0.6' && url.loc.split('.hu/')[1].includes('pizza');
-		});
+		const pizzaUrls = getListOfPizzaUrls(result);
 		return pizzaUrls;
 	} catch (error) {
 		console.error(error);
